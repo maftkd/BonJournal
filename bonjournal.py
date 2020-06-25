86fdcf885fd39912ab317eadcfd42eb57e861131
Michael <michael.feldman09@gmail.com>
2020-06-25 10:36:23 -0400

    Created new python project for journaling

---
+++ bonjournal.py
@@ -0,0 +1,20 @@
+import sys
+import os
+from datetime import date
+from datetime import datetime
+import glob
+
+#colors
+blue = "\033[1;34;34m"
+cyan = "\033[1;36;36m"
+white = "\033[1;37;37m"
+dark_grey = "\033[1;30;30m"
+red = "\033[1;31;31m"
+green = "\033[1;32;32m"
+yellow = "\033[1;33;33m"
+purple = "\033[1;35;35m"
+
+
+#revert to my default theme
+os.system('color 0a')
+os.system('cls')
